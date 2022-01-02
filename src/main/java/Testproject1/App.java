d49528237eb7ab0280afd1ce26259988068c76b2
SaiNarasimha <sainarasimha2010@gmail.com>
2022-01-02 21:13:15 +0530

    maven

---
+++ src/main/java/Testproject1/App.java
@@ -0,0 +1,13 @@
+package Testproject1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
